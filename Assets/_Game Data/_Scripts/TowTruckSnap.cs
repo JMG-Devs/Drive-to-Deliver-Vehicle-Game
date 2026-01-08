82959c5371e2945133100330bbda775e741580f3
HarisRiaz <>
2026-01-08 15:42:41 +0500

    Shadows fixed in level prefabs

--- Assets/_Game Data/_Scripts/TowTruckSnap.cs
+++ Assets/_Game Data/_Scripts/TowTruckSnap.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class TowTruckSnap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform parkingPoint,player;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public bool isLerpStarting = false;
+    public Collider selfCollider; 
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            selfCollider.enabled = false;
+            isLerpStarting = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!isLerpStarting) return;
+
+        // Smooth position
+        player.position = Vector3.Lerp(
+            player.position,
+            parkingPoint.position,
+            Time.deltaTime * moveSpeed
+        );
+
+        // Smooth rotation
+        player.rotation = Quaternion.Lerp(
+            player.rotation,
+            parkingPoint.rotation,
+            Time.deltaTime * rotateSpeed
+        );
+
+        // Finish condition
+        if (Vector3.Distance(player.position, parkingPoint.position) < 0.5/*0.05f*/)
+        {
+            isLerpStarting = false;
+
+            //Destroy(this); // optional optimization
+        }
     }
+
 }
